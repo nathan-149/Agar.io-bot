a9fccaa564a2133956008f20ad80db0c5903d602
Nathan Ang <nathanang2000@gmail.com>
2015-06-14 14:52:38 -0700

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -884,7 +884,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  originalName = "NotReallyABot",
+  originalName = "angrocket",
   sessionScore = 0,
   serverIP = "",
   interNodes = [],
